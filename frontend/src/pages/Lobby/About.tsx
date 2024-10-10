b35d7cde873ab2397e589c3065a82f0b48e07473
AntonCSalvador <antoncsalvador@gmail.com>
2024-10-10 18:27:11 -0400

    added tabler/icons and added start of lobby

--- frontend/src/pages/Lobby/About.tsx
+++ frontend/src/pages/Lobby/About.tsx
@@ -3,21 +3,45 @@ import {
   Button,
   Group,
   Stack,
- 
+  Text,
+  CopyButton
 } from "@mantine/core";
-import '../../index.css'; // Ensure this imports Tailwind styles
+import { IconSettings } from '@tabler/icons-react';
+import '../../index.css';
 
 export default function Layout() {
   return (
     <div className="bg-mafiaBlack-default min-h-screen p-4">
       <Stack justify="center" align="center">
         <Group>
-          <h1>Hi</h1>
+          <Text size="md" color="red">Invite Code: </Text>
+          <CopyButton value="https://www.youtube.com/">
+            {({ copied, copy }) => (
+              <Button color={copied ? '#3E8E7E' : '#E94560'} onClick={copy}>
+                {copied ? 'Copied code' : 'CODE'}
+              </Button>
+            )}
+          </CopyButton>
+          <Button color="#E94560">
+              Theme
+          </Button>
         </Group>
-        <Group>
-          <h1>Hi</h1>
+        <Group className="border border-mafiaRed-default rounded-lg p-4">
+          <Stack>
+            <Group>
+              <Text size="md" color="white">Everyone's Ready</Text>
+              <Button color="#3E8E7E" onClick={() => console.log("hello")}>
+                Start
+              </Button>
+              <Button variant="outline" color="#E94560">
+                <IconSettings size={24} stroke={2} />
+              </Button>
+            </Group>
+          </Stack>
         </Group>        
       </Stack>
+
+
       <h1 className="text-white">About Page</h1>
       <p className="text-white">Learn more about us on this page.</p>
       <Button color="red">
