648cbc22589a4c3f0329dbadbb947db36af5fd50
WilsonGoins <wilson.goins@ufl.edu>
2024-10-11 17:45:33 -0400

    finished task
    finished task

--- frontend/src/pages/DayText/About.tsx
+++ frontend/src/pages/DayText/About.tsx
@@ -1,6 +1,6 @@
 import "@mantine/core/styles.css";
 import { Group, Stack, Text, Image, ScrollArea, Button } from "@mantine/core";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import '../../index.css';
 import GoogleTTS from "../../GoogleTTS";
@@ -11,30 +11,37 @@ export const socket = io("http://localhost:8000"); // Ensure this matches your s
 export default function Layout() {
   const [story, setStory] = useState("");
 
-  // save story
-  socket.on("story-generated", (story) => {
-    console.log(story);
-    setStory(story);
-  });
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server:", socket.id);
+    });
+
+    // save story
+    socket.on("story-generated", (story) => {
+      setStory(story);
+    });
+  }, []);
 
   // generate story via socket listener
   const generateStory = () => {
-    socket.emit("generate-story", 'reirere', ['ryder', 'wilson', 'lazzy'], 'lazzy', 'ryder', 'the beach');
-    console.log("story created!");
+    socket.emit("generate-story", 'exampleCode', ['ryder', 'wilson', 'lazzy'], 'lazzy', 'ryder', 'school');
   }
 
   return (
 
     <div className="bg-mafiaBlack-default min-h-screen items-center">
       <Group justify="center" align="center" gap={50} className="flex flex-col min-h-screen md:flex-row gap-4 w-full border-4 border-blue-500">
-        <Stack align="center" justify="center" className="w-full md:w-1/3 border-4 border-blue-500">
-          <Text size="md" color="white">Theme: L'Checkers</Text>
-          <Image radius="md" src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png" className="border-4 border-mafiaRed-default" />
+        <Stack align="center" justify="center" className="w-full md:w-1/3">
+          <Stack align="center" justify="center" className="w-full border-4 border-blue-500">
+            <Text size="lg" c="white" mt="xs">Theme: L'Checkers</Text>
+            <Image radius="md" mb="xs" mr="xs" ml="xs" src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png" className="border-4 border-mafiaRed-default" />
+          </Stack>
+          <GoogleTTS placeholderText={story} />
         </Stack>
         <Stack mt="sm" className="w-full md:w-1/3 border-4 border-mafiaRed-default rounded-md">
           <Group justify="center" align="center">
               <div className="min-w-[100%] w-[100%] bg-mafiaBlack-default p-4">
-                <GoogleTTS placeholderText={story} />
+                <Text size="xl" c="white">{story}</Text>
               </div>
           </Group>       
         </Stack>
